70be617eb13ef783a1eee682d2df8c9d50d581b3
Vinayak Donawad <78019838+vinayakdon@users.noreply.github.com>
2022-03-13 19:57:44 +0530

    java code

---
+++ project.java
@@ -0,0 +1,25 @@
+package vinayaksproject;
+import java.util.*;
+
+public class project {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        System.out.println("welcome to java programming vinayak");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter the number");
+        int number = sc.nextInt();
+        int number1 = sc.nextInt();
+        
+        System.out.println(number+number1);
+        
+        
+        Scanner 
+        
+        
+        
+        
+
+	}
+
+}
